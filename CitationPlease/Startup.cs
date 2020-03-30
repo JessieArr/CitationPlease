fad22838ff44adf99ffbded3837945fa9a338cfc
ShaWn <smorrison1986@gmail.com>
2020-03-29 21:41:33 -0500

    Updating main page layout. Adding runtime recompilation.

--- CitationPlease/Startup.cs
+++ CitationPlease/Startup.cs
@@ -25,7 +25,7 @@ namespace CitationPlease
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
 
             services.AddScoped<GovInfoClient, GovInfoClient>();
             services.AddScoped<SecretHelper, SecretHelper>();
